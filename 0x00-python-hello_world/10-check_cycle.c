1d7aa33c191bb951589754d915d7a8348242d772
LeTeguis <teuguiasederis@gmail.com>
2022-09-20 01:43:01 +0000

    10. Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,28 +12,37 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *current = 0;
-
-	if (list == 0)
-		return (0);
-	if (list->next == list)
-		return (1);
-	current = list->next;
+	listint_t *current = list;
+	listint_t *nocycle = 0;
+	listint_t *previous = 0;
 
 	while (current != 0)
 	{
-		listint_t *is_current = list;
-		int number_count = 0;
-
-		while (is_current->next != 0)
+		if (nocycle == 0)
 		{
-			if (is_current == current)
-				number_count++;
-			if (number_count > 1)
-				return (1);
-			is_current = is_current->next;
+			previous = current;
+			nocycle = current;
+			current = current->next;
+			nocycle->next = 0;
 		}
-		current = current->next;
+		else
+		{
+			listint_t *is_current = nocycle;
+
+			while (is_current->next != 0 || is_current == current)
+			{
+				if (is_current == current)
+				{
+					previous->next = current;
+					return (1);
+				}
+				is_current = is_current->next;
+			}
+			previous = current;
+			is_current->next = current;
+			current = current->next;
+			is_current->next->next = 0;
+		}
 	}
 	return (0);
 }
