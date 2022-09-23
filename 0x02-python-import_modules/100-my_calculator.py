50320b00f6341172cb87c3bb847cf9204cb3cc63
LeTeguis <teuguiasederis@gmail.com>
2022-09-23 00:29:02 +0000

    send first

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    '''from calculator_1 import add, sub, mul, div'''
+    import calculator_1 as cal
     from sys import argv
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        operator = {"+": add, "-": sub, "*": mul, "/": div}
+        operator = {"+": cal.add, "-": cal.sub, "*": cal.mul, "/": cal.div}
         a = int(argv[1])
         b = int(argv[3])
         o = argv[2]
