e65ecd3237c11fd05b942413ec54a41fb186e2d7
LeTeguis <teuguiasederis@gmail.com>
2022-09-23 00:25:02 +0000

    send first

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1 as cal
+    from calculator_1 import add, sub, mul, div
     from sys import argv
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        operator = {"+": cal.add, "-": cal.sub, "*": cal.mul, "/": cal.div}
+        operator = {"+": add, "-": sub, "*": mul, "/": div}
         a = int(argv[1])
         b = int(argv[3])
         o = argv[2]
         if o in operator:
-            print("{} {} {} = {}".format(a, o, b, operator[o](a, b)))
+            print("{:d} {:d} {:d} = {:d}".format(a, o, b, operator[o](a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
