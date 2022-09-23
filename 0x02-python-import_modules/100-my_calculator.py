33b1bad319f50c3129d8aff2c6e22edc5d5c9bc1
LeTeguis <teuguiasederis@gmail.com>
2022-09-23 00:20:47 +0000

    send first

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import calculator_1 as cal
-from sys import argv
+if __name__ == "__main__":
+    import calculator_1 as cal
+    from sys import argv
 
-if len(argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-else:
-    operator = {"+": cal.add, "-": cal.sub, "*": cal.mul, "/": cal.div}
-    a = int(argv[1])
-    b = int(argv[3])
-    o = argv[2]
-    if o in operator:
-        print("{} {} {} = {}".format(a, o, b, operator[o](a, b)))
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
+        operator = {"+": cal.add, "-": cal.sub, "*": cal.mul, "/": cal.div}
+        a = int(argv[1])
+        b = int(argv[3])
+        o = argv[2]
+        if o in operator:
+            print("{} {} {} = {}".format(a, o, b, operator[o](a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
