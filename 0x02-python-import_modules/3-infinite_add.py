983fe267a5b361da9b9004aa2093b688f7aa9fdd
LeTeguis <teuguiasederis@gmail.com>
2022-09-23 00:14:07 +0000

    send first

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-somme = 0
+    somme = 0
 
-if len(argv) > 1:
-    for i in range(1, len(argv)):
-        somme += int(argv[i])
-print("{}".format(somme))
+    if len(argv) > 1:
+        for i in range(1, len(argv)):
+            somme += int(argv[i])
+    print("{:d}".format(somme))
