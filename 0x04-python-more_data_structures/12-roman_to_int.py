06a83e4c065e0a34e8d3a1ca3ca19f1ac8a8f4db
LeTeguis <teuguiasederis@gmail.com>
2022-09-28 16:05:59 +0000

    update 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 
+U = ["IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I"]
+T = ["XC", "LXXX", "LXX", "LX", "L", "XL", "XXX", "XX", "X"]
+H = ["CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C"]
+TH = ["MMM", "MM", "M"]
+Units = {U[i]: (9 - i) for i in range(9)}
+Tens = {T[i]: (9 - i) * 10 for i in range(9)}
+Hundreds = {H[i]: (9 - i) * 100 for i in range(9)}
+Thousands = {TH[i]: (3 - i) * 1000 for i in range(3)}
+GIR = [U, T, H, TH]
+GRI = [Units, Tens, Hundreds, Thousands]
+
 def int_to_roman(valeur):
     if valeur is None or type(valeur) is not int:
         return '0'
-    U = ["IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I"]
-    T = ["XC", "LXXX", "LXX", "LX", "L", "XL", "XXX", "XX", "X"]
-    H = ["CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C"]
-    TH = ["MMM", "MM", "M"]
-    G = [U, T, H, TH]
-
     result = ''
     if valeur == 0:
         result = '0'
@@ -17,7 +22,7 @@ def int_to_roman(valeur):
         while valeur != 0:
             p = (9 if i != 3 else 3) - valeur % 10
             if p != 9:
-                v = G[i][p]
+                v = GIR[i][p]
                 result = f"{v}{result}"
             valeur = int(valeur / 10)
             i += 1
@@ -32,21 +37,11 @@ def roman_to_int(roman_string):
         return 0
     if not roman_string.isupper():
         return 0
-    V = [9, 8, 7, 6, 5, 4, 3, 2, 1]
-    U = ["IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I"]
-    T = ["XC", "LXXX", "LXX", "LX", "L", "XL", "XXX", "XX", "X"]
-    H = ["CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C"]
-    TH = ["MMM", "MM", "M"]
-    Units = {U[i]: V[i] for i in range(9)}
-    Tens = {T[i]: V[i] * 10 for i in range(9)}
-    Hundreds = {H[i]: V[i] * 100 for i in range(9)}
-    Thousands = {TH[i]: V[6 + i] * 1000 for i in range(3)}
-    resume = [Units, Tens, Hundreds, Thousands]
     rs = ""
     for e in roman_string:
         rs = f"{rs}{e}"
     valeur = 0
-    for dom in resume:
+    for dom in GRI:
         for k, v in dom.items():
             if k in rs:
                 if k == "V" and "IV" in rs:
