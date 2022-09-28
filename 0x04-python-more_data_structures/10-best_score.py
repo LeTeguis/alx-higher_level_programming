5b7203a16d5265824c0f1d492ab581ce5227d362
LeTeguis <teuguiasederis@gmail.com>
2022-09-28 09:58:07 +0000

    update 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None or len(a_dictionary) <= 0:
+    if a_dictionary is None or len(a_dictionary) <= 0:
         return None
     max_key = list(a_dictionary)[0]
     for k, v in a_dictionary.items():
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if roman_string == None or type(roman_string) is not str:
+    if roman_string is None or type(roman_string) is not str:
         return 0
-    resume = {"Units": {"IX": 9, "VIII": 8, "VII": 7, "VI": 6, "V": 5,
-                "IV": 4, "III": 3, "II": 2, "I": 1},
-                "Tens": {"XC": 90, "LXXX": 80, "LXX": 70, "LX": 60, "L": 50,
-                "XL": 40, "XXX": 30, "XX": 20, "X": 10},
-                "Hundreds": {"CM": 900, "DCCC": 800, "DCC": 700, "DC": 600,
-                "D": 500, "CD": 400, "CCC": 300, "CC": 200, "C": 100},
-                "Thousands": {"MMM": 3000, "MM": 2000, "M": 1000}}
+    Units = {"IX": 9, "VIII": 8, "VII": 7, "VI": 6, "V": 5, "IV": 4, "III": 3, "II": 2, "I": 1}
+    Tens = {"XC": 90, "LXXX": 80, "LXX": 70, "LX": 60, "L": 50, "XL": 40, "XXX": 30, "XX": 20, "X": 10}
+    Hundreds = {"CM": 900, "DCCC": 800, "DCC": 700, "DC": 600, "D": 500, "CD": 400, "CCC": 300, "CC": 200, "C": 100}
+    Thousands = {"MMM": 3000, "MM": 2000, "M": 1000}
+    resume = {"Units": Units, "Tens": Tens, "Hundreds": Hundreds, "Thousands": Thousands}
     rs = ""
     for e in roman_string:
         rs = f"{rs}{e}"
@@ -18,6 +16,6 @@ def roman_to_int(roman_string):
         for k, v in v1.items():
             if k in rs:
                 valeur = valeur + v
-                rs = rs.replace(k,'*')
+                rs = rs.replace(k, '*')
                 break
     return valeur
