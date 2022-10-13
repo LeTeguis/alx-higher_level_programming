4f7cc9831796ff295d3a7371d9ee6286f961ddc5
LeTeguis <teuguiasederis@gmail.com>
2022-10-13 03:43:17 +0000

    up

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""0-square
+"""
+
+
+class Square:
+    """Square
+    """
+    __size = 0
+
+    def __init__(self, size=0):
+        """
+        Note:
+            do not include self parameter
+        Args:
+            size (int): size of Square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Note:
+            none
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """int: getter
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """int: setter
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """
+        Note: print the graphics square
+        """
+        if self.__size > 0:
+            for i in range(self.__size):
+                print("#" * self.__size)
+        else:
+            print()
