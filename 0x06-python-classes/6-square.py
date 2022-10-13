5c72606a4c711a540e7e05a77955d627bb77d1ac
LeTeguis <teuguiasederis@gmail.com>
2022-10-13 03:56:48 +0000

    up

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""0-square
+"""
+
+
+class Square:
+    """Square
+    """
+    __size = 0
+    __position = (0, 0)
+
+    def __init__(self, size=0, position = (0, 0)):
+        """
+        Note:
+            do not include self parameter
+        Args:
+            size (int): size of Square
+            position (tuple): 2 elements
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.position = position
+
+    def area(self):
+        """
+        Note:
+            none
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """int: getter
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """int: setter
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """
+        Note: print the graphics square
+        """
+        if self.__size > 0:
+            for i in range(self.__size):
+                print("{}{}".format(" " * self.__position[0],"#" * self.__size))
+        else:
+            print()
+
+    @property
+    def position(self):
+        """
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        """
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
