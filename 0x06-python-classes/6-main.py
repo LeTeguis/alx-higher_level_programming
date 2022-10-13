d39692a5223060a26d83d7dbad56c26d8c9f245a
LeTeguis <teuguiasederis@gmail.com>
2022-10-13 04:12:45 +0000

    up

--- 0x06-python-classes/6-main.py
+++ 0x06-python-classes/6-main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 Square = __import__('6-square').Square
 
+my_square = Square(3, (1, "3"))
+my_square.my_print()
+
+print("--")
+
 my_square_1 = Square(3)
 my_square_1.my_print()
 
