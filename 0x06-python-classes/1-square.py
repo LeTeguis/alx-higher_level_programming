1189d406e7075dae19ee4072924e4c7df8668e42
LeTeguis <teuguiasederis@gmail.com>
2022-10-13 02:54:32 +0000

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""0-square
+"""
+
+
+class Square:
+    """Square
+    """
+    __size = 0
+
+    def __init__(self, size):
+        """
+        Note:
+            do not include self parameter
+        Args:
+            size (int): size of Square
+        """
+        self.__size = size
